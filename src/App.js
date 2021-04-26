0e3c389d6c17ed4d746b454c1c8a48e9b67eecfd
arief sangga <arief.heaven@gmail.com>
2021-04-26 21:28:48 +0700

    menamba menmpilkan daftar produk dari api

--- src/App.js
+++ src/App.js
@@ -1,28 +1,64 @@
 // import logo from './logo.svg';
 // import './App.css';
+import React, { Component } from 'react'
 import { Row, Col, Container } from "react-bootstrap";
-import { Hasil, ListCategories, NavbarComponent } from "./components";
+import { Hasil, ListCategories, Menus, NavbarComponent } from "./components";
+import { API_URL } from './utils/constants'
+import axios from 'axios'
 // import ListCategories from './components/ListCategories';
 // import NavbarComponent from './components/NavbarComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <NavbarComponent />
-      <div className="mt-3">
-        <Container fluid>
-          <Row>
-            <ListCategories />
-            <Col>
-              <h4><strong> Daftar Produk </strong></h4>
-              <hr />
-            </Col>
-            <Hasil />
-          </Row>
-        </Container>
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      menus: [],
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get(API_URL + "products")
+      .then(res => {
+        // console.log("Response : ", res);
+        const menus = res.data;
+        this.setState({ menus });
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
+  render() {
+    const { menus } = this.state
+    // console.log(this.state.menus);
+    return (
+      <div className="App">
+        <NavbarComponent />
+        <div className="mt-3">
+          <Container fluid>
+            <Row>
+              <ListCategories />
+              <Col>
+                <h4><strong> Daftar Produk </strong></h4>
+                <hr />
+                <Row>
+                  {menus && menus.map((menu) => (
+                    <Menus
+                      key={menu.id}
+                      menu={menu}
+                    />
+                  ))}
+                </Row>
+              </Col>
+              <Hasil />
+            </Row>
+          </Container>
+        </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
 
-export default App;
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@
 import Hasil from './Hasil'
 import ListCategories from './ListCategories'
 import NavbarComponent from './NavbarComponent'
+import Menus from './Menus'
 
-export { Hasil, ListCategories, NavbarComponent }
+export { Hasil, ListCategories, NavbarComponent, Menus }
