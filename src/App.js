462176d1594c88befc2f8af9bef22b9400906c35
arief sangga <arief.heaven@gmail.com>
2021-05-03 11:03:01 +0700

    menambahkan menu hasil, tambah dan update data

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Row, Col, Container } from "react-bootstrap";
 import { Hasil, ListCategories, Menus, NavbarComponent } from "./components";
 import { API_URL } from './utils/constants'
 import axios from 'axios'
+import swal from 'sweetalert'
 // import ListCategories from './components/ListCategories';
 // import NavbarComponent from './components/NavbarComponent';
 
@@ -15,13 +16,14 @@ export default class App extends Component {
 
     this.state = {
       menus: [],
-      categoriYangDipilih: 'Makanan'
+      categoriYangDipilih: 'Makanan',
+      keranjangs: []
     }
   }
 
   componentDidMount() {
     axios
-      .get(API_URL + "products?category.nama="+this.state.categoriYangDipilih)
+      .get(API_URL + "products?category.nama=" + this.state.categoriYangDipilih)
       .then(res => {
         // console.log("Response : ", res);
         const menus = res.data;
@@ -29,17 +31,41 @@ export default class App extends Component {
       })
       .catch(error => {
         console.log(error);
+      });
+
+    axios
+      .get(API_URL + "keranjangs")
+      .then(res => {
+        const keranjangs = res.data;
+        this.setState({ keranjangs });
       })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  componentDidUpdate(prevState) {
+    if (this.state.keranjangs !== prevState.keranjangs) {
+      axios
+        .get(API_URL + "keranjangs")
+        .then(res => {
+          const keranjangs = res.data;
+          this.setState({ keranjangs });
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
   }
 
   changeCategory = (value) => {
     this.setState({
       categoriYangDipilih: value,
-      menus : []
+      menus: []
     })
 
     axios
-      .get(API_URL + "products?category.nama="+value)
+      .get(API_URL + "products?category.nama=" + value)
       .then(res => {
         // console.log("Response : ", res);
         const menus = res.data;
@@ -50,8 +76,64 @@ export default class App extends Component {
       })
   }
 
+  masukKeranjang = (value) => {
+
+    axios
+      .get(API_URL + "keranjangs?product.id=" + value.id)
+      .then(res => {
+        if (res.data.length === 0) {
+          const keranjang = {
+            jumlah: 1,
+            total_harga: value.harga,
+            product: value
+          };
+
+          axios
+            .post(API_URL + "keranjangs", keranjang)
+            .then(res => {
+              swal({
+                title: "Sukses Masuk Keranjan",
+                text: "Sukses Masuk Keranjan" + keranjang.product.nama,
+                icon: "success",
+                button: false,
+                timer: 1500,
+              });
+            })
+            .catch(error => {
+              console.log(error);
+            });
+        } else {
+
+          const keranjang = {
+            jumlah: res.data[0].jumlah + 1,
+            total_harga: res.data[0].total_harga + value.harga,
+            product: value,
+          };
+
+          axios
+            .put(API_URL + "keranjangs/" + res.data[0].id, keranjang)
+            .then((res) => {
+              swal({
+                title: "Sukses Masuk Keranjan",
+                text: "Sukses Masuk Keranjan" + keranjang.product.nama,
+                icon: "success",
+                button: false,
+                timer: 1500,
+              });
+            })
+            .catch(error => {
+              console.log(error);
+            });
+
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  };
+
   render() {
-    const { menus, categoriYangDipilih } = this.state
+    const { menus, categoriYangDipilih, keranjangs } = this.state
     // console.log(this.state.menus);
     return (
       <div className="App">
@@ -68,11 +150,12 @@ export default class App extends Component {
                     <Menus
                       key={menu.id}
                       menu={menu}
+                      masukKeranjang={this.masukKeranjang}
                     />
                   ))}
                 </Row>
               </Col>
-              <Hasil />
+              <Hasil keranjangs={keranjangs} />
             </Row>
           </Container>
         </div>
